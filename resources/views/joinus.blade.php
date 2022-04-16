3486f085acb625ad087353a812806c3e9571b774
RoisyalBariz <74692525+RoisyalBariz@users.noreply.github.com>
2022-04-16 23:37:13 +0700

    Revisi

--- resources/views/joinus.blade.php
+++ resources/views/joinus.blade.php
@@ -4,13 +4,13 @@
     <h1 class="text-center">Category Partnership</h1>
     <h5 class="text-center">Silahkan Pilih Katerogi kerjasama</h5>
     <div class="row p-1">
-        <a href=""><img class="mx-auto d-block" src="/source/img/peneliti.png" alt=""></a>
+        <a href=""><img style="width: 450px; height:auto; " class="mx-auto d-block" src="/source/img/peneliti.png" alt=""></a>
     </div>
     <div class="row p-1">
-        <a href=""><img class="mx-auto d-block" src="/source/img/developer.png" alt=""></a>
+        <a href=""><img style="width: 450px; height:auto; " class="mx-auto d-block" src="/source/img/developer.png" alt=""></a>
     </div>
     <div class="row p-1">
-        <a href=""><img class="mx-auto d-block" src="/source/img/sponsor.png" alt=""></a>
+        <a href=""><img style="width: 450px; height:auto; " class="mx-auto d-block" src="/source/img/sponsor.png" alt=""></a>
     </div>
 </div>
 @endsection
